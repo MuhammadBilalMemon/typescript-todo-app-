299503f8552bc125d330eeaa38ebe0df4fbc575e
Muhammad Bilal <billu97.memon@gmail.com>
2020-07-25 21:35:14 +0500

    TodoLiistItem component Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoListItem from "./TodoListItem";
 
 function App() {
+  const initialTodos: Todo[] = [
+    { text: "Walk the dog", complete: false },
+    { text: "Write App", complete: true },
+  ];
+
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodo = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+        <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
+      </ul>
+    </>
   );
 }
 
